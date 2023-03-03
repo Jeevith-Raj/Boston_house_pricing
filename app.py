762a1d5f11ba5bb82915400ad286f6c1855fab67
Jeevith-raj <20uad028@kamarajengg.edu.in>
2023-03-03 22:13:48 +0530

    with post api

---
+++ app.py
@@ -0,0 +1,33 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+##load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/pup')
+def home(): 
+    return render_template('home.html')
+
+@app.route('/predict_api' , methods=['POST'])
+
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.arraylist(data.values())).reshape(1,-1)
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(newdata)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+      
+     
